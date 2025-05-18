86a9b2e88aa48b656f497310628340dfb6d09f6f
Kitlizin <148634979+Kitlizin@users.noreply.github.com>
2025-05-18 14:56:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from PIL import Image
 import os
 
 # Load YOLOv8 model
-model = YOLO("byolov8_reckless9.pt")
+model = YOLO("yolov8_reckless9.pt")
 
 # Custom styling
 st.set_page_config(page_title="🚦 Reckless Driving Detector", layout="centered")
